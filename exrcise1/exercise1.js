6b8360c835cc4fb982d2169c33ee7d88491238a1
rebertionfire <joaoreberti@gmail.com>
2019-12-17 19:16:37 +0000

    exercise1 done, agora sim

--- exrcise1/exercise1.js
+++ exrcise1/exercise1.js
@@ -4,7 +4,7 @@ const numeros = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
 const batalhaNaval = () =>{
     for(let i=0; i<letras.length;i++){
-        mapa[i] = letras[i] + numeros[i]
+        mapa[i] = letras[i] +'-'+ numeros[i]
     } 
      console.log (mapa)
 }
